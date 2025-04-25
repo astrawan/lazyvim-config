962920889eb2c77d469fc66941ffcc20cff29369
Astrawan Wayan <astra@pm.me>
2025-04-26 04:58:58 +0800

    feat: add denols and copilot dep
    - Add denols to LSP config
    - Add copilot.lua to code companion
    - Enable latex feature

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -2,6 +2,7 @@ return {
   "olimorris/codecompanion.nvim",
   opts = {},
   dependencies = {
+    "zbirenbaum/copilot.lua",
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
