d3c41cea4da0175ea8d15532675e3753eba88277
Astrawan Wayan <astra@pm.me>
2025-08-01 18:48:41 +0800

    feat: lsp meson filter by os and distro

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,9 +1,35 @@
+local sysname = string.lower(vim.uv.os_uname().sysname)
+local is_nixos = nil
+
+if sysname == "linux" then
+  local fd_os_release = assert(io.open("/etc/os-release"), "r")
+  local s_os_release = fd_os_release:read("*a")
+  fd_os_release:close()
+  s_os_release = s_os_release:lower()
+  is_nixos = s_os_release:match("nixos")
+end
+
+if is_nixos == nil then
+  print('is not nixos')
+else
+  print('is nixos')
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
       denols = {
         enabled = true,
+      },
+      texlab = {
+        mason = is_nixos == nil,
+      },
+      lua_ls = {
+        mason = is_nixos == nil,
+      },
+      marksman = {
+        mason = is_nixos == nil,
       },
     },
   },
